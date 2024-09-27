6eeb8e26354be0c4766544ab040cbee7a1bc6557
EloiseHelena <eloisehelena2014@gmail.com>
2024-09-26 23:33:57 -0300

    Implementando DOM

---
+++ frontend/js/app.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('task-form');
+const taskList = document.getElementById('task-list');
+const filterStatus = document.getElementById('filter-status');
+const filterCategory = document.getElementById('filter-category');
+const filterPriority = document.getElementById('filter-priority');
