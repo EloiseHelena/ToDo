e762e3933719ce1c6ccfb215ed7ef867f958b28e
EloiseHelena <eloisehelena2014@gmail.com>
2024-09-26 23:36:27 -0300

    Implementando Json e LocalStorage

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,3 +3,5 @@ const taskList = document.getElementById('task-list');
 const filterStatus = document.getElementById('filter-status');
 const filterCategory = document.getElementById('filter-category');
 const filterPriority = document.getElementById('filter-priority');
+
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
